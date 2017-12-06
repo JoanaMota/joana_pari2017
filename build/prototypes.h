2feb9b6fd9a8569a4fec7230c6b085412c85c98f
Joana <joancarvalhomota@ua.pt>
2017-12-06 12:32:45 +0000

    complete all functions to star image processing

---
+++ build/prototypes.h
@@ -0,0 +1,11 @@
+/*File generated automatically in joana-X541UV by joana on Qua Dez  6 12:06:48 WET 2017*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+/* CMakeFiles/3.5.1/CompilerIdC/CMakeCCompilerId.c */
+/* CMakeFiles/feature_tests.c */
+#ifdef __cplusplus
+}
+#endif
+/* CMakeFiles/3.5.1/CompilerIdCXX/CMakeCXXCompilerId.cpp */
+/* CMakeFiles/feature_tests.cxx */
